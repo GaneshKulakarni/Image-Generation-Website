a92853b74a7cd8d108a8ecac2f663ae616c3db91
Ganesh Kulakarni <ganeshkulakarni@gmail.com>
2025-07-02 23:48:03 +0530

    Added BuyCredits ,Login

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Result from './pages/Result'
 import BuyCredits from './pages/BuyCredits'
 import Navbar from './componenets/Navbar'
 import Footer from './componenets/Footer'
+import Login from './componenets/Login'
 function App() {
 
   return (
     <>
      <div className='px-4 sm-px-18 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-500 to-orange-50' >
       <Navbar/>
+      <Login/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/result' element={<Result/>}/>
